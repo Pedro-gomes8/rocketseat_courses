4a028614bc2835487cd3b84775c18ab61a720ded
Pedro Gomes <pedro.gomes@poli.ufrj.br>
2021-04-06 16:40:35 -0300

    A bit of functions - the js way

--- JavaScript_training/func.js
+++ JavaScript_training/func.js
@@ -1,6 +1,29 @@
 // Function anonymous
+// Funcao 
+
 const a = function (a1,a2) {
     return a1+a2
 }
 
 console.log(a(2,3))
+
+// function hoisting
+aloGaleraDePeao()
+function aloGaleraDePeao(AloGaleradeCowboy) {
+  console.log('musica que ta na cabeca agora por n sei qual motivo')  
+};
+// THAT WORKS!
+
+// Arrow function
+const sayMyNamy = () => {
+    console.log('Pedro')
+};
+sayMyNamy()
+
+// Function constructor
+function New_class(nome) {
+    this.nome = nome;
+};
+
+const objeto= new New_class("Pedro")
+console.log(objeto)
