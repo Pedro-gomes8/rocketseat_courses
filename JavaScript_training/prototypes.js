22acda60cbf12404b1ef882402bf4b28a13d55fb
Pedro Gomes <pedro.gomes@poli.ufrj.br>
2021-04-20 21:34:11 -0300

    Small change, understanding prototypes

---
+++ JavaScript_training/prototypes.js
@@ -0,0 +1,12 @@
+/* 
+    Prototypes
+    __proto__
+  
+    Ver todos os metodos atraves de .__proto__
+*/
+
+let string='Calafrio';
+let numero= 345784;
+console.log(`A palavra ${string} possui ${string.length} caracteres\n
+O Numero ${numero} possui ${String(numero).length} digitos
+`);
